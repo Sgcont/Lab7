f7d7f68a2b5ae014b3b1b1bda722e7edb32032aa
Lucas de Araujo <lucas@contreiras.info>
2026-06-19 19:24:34 -0300

    AlbumTeste.java

---
+++ AlbumTeste.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AlbumTeste {
+
+    private Album<Figurinha> album;
+
+}
